f02c649c5342cfba1c493673aa045a877dbeaf8d
Maria Quinton <maria.quinton28@gmail.com>
2020-11-09 22:06:16 -0600

    Finished with hw - working on bonus

--- query_schema.sql
+++ query_schema.sql
@@ -1,17 +1,67 @@
 -- 1.List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT staff.emp_no,
+  staff.first_name,
+  staff.last_name,
+  staff.sex,
+  salary.salary
+FROM staff
+INNER JOIN salary ON
+staff.emp_no = salary.emp_no;
+
 -- 2.List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date FROM staff
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- 3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT mang.emp_no,
+  mang.dept_no,
+  staff.first_name,
+  staff.last_name,
+  department.dept_name
+FROM dept_manager as mang
+INNER JOIN staff ON
+mang.emp_no = staff.emp_no
+INNER JOIN department ON
+mang.dept_no = department.dept_no;
+
 -- 4.List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT staff.emp_no,
+  staff.first_name,
+  staff.last_name,
+  department.dept_name
+FROM staff
+INNER JOIN dept_staff ON
+staff.emp_no = dept_staff.emp_no
+INNER JOIN department ON
+dept_staff.dept_no = department.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex FROM staff
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- 6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT staff.emp_no,
+  staff.first_name,
+  staff.last_name,
+  department.dept_name
+FROM staff
+INNER JOIN dept_staff ON
+staff.emp_no = dept_staff.emp_no
+INNER JOIN department ON
+dept_staff.dept_no = department.dept_no
+WHERE department.dept_name = 'Sales';
+
 -- 7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT staff.emp_no,
+  staff.first_name,
+  staff.last_name,
+  department.dept_name
+FROM staff
+INNER JOIN dept_staff ON
+staff.emp_no = dept_staff.emp_no
+INNER JOIN department ON
+dept_staff.dept_no = department.dept_no
+WHERE department.dept_name IN ('Sales', 'Development');
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name,
--- table_schema.sql
+++ table_schema.sql
@@ -9,50 +9,39 @@ CREATE TABLE department (
 );
 
 CREATE TABLE dept_staff (
-    emp_no BIGINT NOT NULL PRIMARY KEY,
-    dept_no VARCHAR(20) NOT NULL
+    emp_no BIGINT NOT NULL,
+    dept_no VARCHAR(20) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES department(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES staff(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 	);
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(20) NOT NULL PRIMARY KEY,
+    dept_no VARCHAR(20) NOT NULL,
     emp_no BIGINT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES department(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES dept_staff(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES staff(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE staff (
     emp_no BIGINT NOT NULL PRIMARY KEY,
-    employee_title_id VARCHAR(30) NOT NULL,
+    emp_title_id VARCHAR(30) NOT NULL,
     birthday DATE NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     sex VARCHAR(30) NOT NULL,
-    hire_date DATE NOT NULL
+    hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES title(emp_title_id)
 );
 
 CREATE TABLE salary (
     emp_no BIGINT NOT NULL PRIMARY KEY,
-	FOREIGN KEY (emp_no) REFERENCES dept_staff (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES staff(emp_no),
     salary BIGINT NOT NULL
 );
 
 CREATE TABLE title (
-   employee_title_id VARCHAR(50) NOT NULL PRIMARY KEY,
+   emp_title_id VARCHAR(50) NOT NULL PRIMARY KEY,
    title VARCHAR(50) NOT NULL
 );
-
--- 2.List first name, last name, and hire date for employees who were hired in 1986.
-SELECT first_name, last_name, hire_date FROM staff
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
-
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-SELECT first_name, last_name, sex FROM staff
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
-
---8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name,
-COUNT(last_name) AS "frequency"
-FROM staff
-GROUP BY last_name
-ORDER BY
-COUNT(last_name) DESC;
